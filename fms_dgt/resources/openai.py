f854962ac5902e2001ff6844632e0bc2b93c6a8e
Max Crouse <maxcrouse@Maxs-MacBook-Pro.local>
2024-07-03 12:13:46 -0500

    name change

---
+++ fms_dgt/resources/openai.py
@@ -0,0 +1,17 @@
+# Standard
+from typing import Dict
+
+# First Party
+from fms_dgt.base.registry import register_resource
+from fms_dgt.resources.api import ApiKeyResource
+
+
+@register_resource("openai")
+class OpenAIKeyResource(ApiKeyResource):
+
+    OPEN_AI_CALL_LIMIT = 10
+
+    def __init__(
+        self, key_name: str = "OPENAI_API_KEY", call_limit: int = OPEN_AI_CALL_LIMIT
+    ):
+        super().__init__(key_name, call_limit)
