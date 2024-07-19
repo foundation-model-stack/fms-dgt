7e6d15a68bfe33a22f81ed5b5077fa76cd02a7f4
Siva Sankalp <7344617+sivasankalpp@users.noreply.github.com>
2024-07-19 14:37:42 -0400

    FileDataloader example (#37)
    * fix: pass qna.yaml top-level seed_examples to FileDataloader
    
    Signed-off-by: Siva Sankalp <isivasankalp@gmail.com>
    
    * feat: add example for FileDataloader
    
    Signed-off-by: Siva Sankalp <isivasankalp@gmail.com>
    
    * add file dataloader tests
    
    Signed-off-by: Siva Sankalp <isivasankalp@gmail.com>
    
    * run formatting
    
    Signed-off-by: Siva Sankalp <isivasankalp@gmail.com>
    
    * remove unused imports
    
    Signed-off-by: Siva Sankalp <isivasankalp@gmail.com>
    
    * fix: update FileDataloader and examples
    
    Signed-off-by: Siva Sankalp <isivasankalp@gmail.com>
    
    * make seed_examples part of dl_args
    
    Signed-off-by: Siva Sankalp <isivasankalp@gmail.com>
    
    * fix: FileDataloader bug
    
    Signed-off-by: Siva Sankalp <isivasankalp@gmail.com>
    
    ---------
    
    Signed-off-by: Siva Sankalp <isivasankalp@gmail.com>
    Signed-off-by: Max Crouse <maxcrouse@Maxs-MacBook-Pro.local>

---
+++ fms_dgt/dataloaders/file.py
@@ -0,0 +1,51 @@
+# Standard
+from typing import Any, Dict, List
+import json
+import os
+
+# Third Party
+import yaml
+
+# Local
+from fms_dgt.base.dataloader import DATA_PATH_KEY
+from fms_dgt.base.registry import register_dataloader
+from fms_dgt.dataloaders.default import DefaultDataloader
+
+from fms_dgt.utils import sdg_logger
+
+
+@register_dataloader("file")
+class FileDataloader(DefaultDataloader):
+    """Class for all json/yaml datasets"""
+
+    def __init__(
+        self,
+        data_path: str = None,
+        seed_examples: List[Any] = None,
+    ) -> None:
+
+        if seed_examples is None:
+            seed_examples = []
+        assert (
+            data_path is not None
+        ), f"{DATA_PATH_KEY} must be set in dataloader specification"
+        if data_path.endswith(".json"):
+            with open(data_path, "r") as f:
+                data = json.load(f)
+        elif data_path.endswith(".yaml"):
+            with open(data_path, "r") as f:
+                data = list(yaml.safe_load(f))
+
+        assert type(data) == list, "Data used for FileDataloader must be a list!"
+
+        data = seed_examples + data
+
+        sdg_logger.info(
+            "Loaded %i seed examples",
+            len(data),
+        )
+
+        super().__init__(data=data)
+
+    def __next__(self):
+        return super().__next__()
