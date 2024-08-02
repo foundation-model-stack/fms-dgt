7de814256ebdf031f772cb19c0d8822014612b3a
Max Crouse <maxwell.crouse@ibm.com>
2024-08-02 13:46:16 -0500

    merge
    Signed-off-by: Max Crouse <maxwell.crouse@ibm.com>

--- fms_dgt/base/databuilder.py
+++ fms_dgt/base/databuilder.py
@@ -2,7 +2,7 @@
 from abc import ABC
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Any, Iterable, List, Mapping, Optional, Type, Union
+from typing import Any, Iterable, List, Mapping, Optional, Union
 import json
 import os
 import time
