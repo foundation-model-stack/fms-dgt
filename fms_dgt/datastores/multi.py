08fd574fe5a1660051c729ada0a64bd95cc9a72d
Max Crouse <maxwell.crouse@ibm.com>
2024-09-18 09:31:31 -0500

    fix multi ds (#102)
    Signed-off-by: Max Crouse <maxwell.crouse@ibm.com>
    Signed-off-by: Maxwell Crouse Maxwell.Crouse@ibm.com <mvcrouse@cccxc589.pok.ibm.com>

--- fms_dgt/datastores/multi.py
+++ fms_dgt/datastores/multi.py
@@ -43,51 +43,17 @@ class MultiTargetDatastore(BaseDatastore):
         return self._datastores
 
     def save_data(self, new_data: List[T]) -> None:
-        "Saves generated data to specified location"
+        """Saves generated data to specified location"""
         for datastore in self._datastores:
             datastore.save_data(new_data)
 
     def load_data(
         self,
     ) -> List[T]:
-        "Loads generated data from primary datastore"
+        """Loads generated data from primary datastore"""
         return self._primary_datastore.load_data()
 
-    def load_dataset(
-        self,
-    ) -> List[T]:
-        "Loads dataset from specified source"
-        return self._primary_datastore.load_dataset()
-
-    def save_task(
-        self,
-    ) -> None:
-        "Default method for saving task specification"
+    def close(self) -> None:
+        """Closes all datastores"""
         for datastore in self._datastores:
-            datastore.save_task()
-
-    def load_task(
-        self,
-    ) -> Any:
-        "Default method for loading task specification"
-        return self._primary_datastore.load_task()
-
-    def save_state(self, state: Any) -> None:
-        "Saves a state object that can be used to restore an object (e.g., a dataloader) to a previous state"
-        for datastore in self._datastores:
-            datastore.save_state(state)
-
-    def load_state(
-        self,
-    ) -> Any:
-        "Loads the state object"
-        return self._primary_datastore.load_state()
-
-    def save_instruction_data(self, new_data: List[T]) -> None:
-        "Saves instruction data to specified location"
-        for datastore in self._datastores:
-            datastore.save_instruction_data(new_data)
-
-    def save_log_data(self, **kwargs):
-        for datastore in self._datastores:
-            datastore.save_log_data(**kwargs)
+            datastore.close()
