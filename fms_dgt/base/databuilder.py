37f498a542c1621cd94bf8098cf1b645a0138f9c
Max Crouse <maxwell.crouse@ibm.com>
2024-09-25 10:38:15 -0500

    add task cards to val
    Signed-off-by: Max Crouse <maxwell.crouse@ibm.com>

--- fms_dgt/base/databuilder.py
+++ fms_dgt/base/databuilder.py
@@ -74,11 +74,11 @@ class DataBuilder(ABC):
             max_stalled_requests if max_stalled_requests is not None else float("inf")
         )
 
-        # initializing generators / validators
-        self._init_blocks()
-
         # initialize tasks
         self._init_tasks(task_kwargs)
+
+        # initializing generators / validators
+        self._init_blocks()
 
         self.kwargs = kwargs
 
@@ -135,7 +135,9 @@ class DataBuilder(ABC):
                 block.name == obj_name for block in self._blocks
             ), f"Duplicate '{obj_name}' block in '{self.name}' data builder"
 
-            obj = get_block(obj_type, **obj_kwargs)
+            task_cards = [t.task_card for t in self._tasks]
+
+            obj = get_block(obj_type, task_cards=task_cards, **obj_kwargs)
 
             # we type check when not using a pipeline
             type_annotations = all_annotations(type(self))
