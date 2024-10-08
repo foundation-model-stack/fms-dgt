4dde7728a658c8b8e33404e86ba34d88fc8053e4
antonpibm <51074867+antonpibm@users.noreply.github.com>
2024-10-08 16:05:21 +0300

    Support json file loading and remove empty loaded entry (#126)
    Signed-off-by: antonp <antonp@il.ibm.com>
    Signed-off-by: Maxwell Crouse Maxwell.Crouse@ibm.com <mvcrouse@cccxc589.pok.ibm.com>

--- fms_dgt/utils.py
+++ fms_dgt/utils.py
@@ -268,7 +268,7 @@ def read_data_file(file_path: str):
             **{
                 "data_builder": "simple",
                 "created_by": "",
-                "seed_examples": [dict()],
+                "seed_examples": [],
             },
             **contents,
         }
