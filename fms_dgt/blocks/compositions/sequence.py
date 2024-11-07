7db7149b14245eacdfc9d1feb1d1870a8701c5fe
Max Crouse <maxwell.crouse@ibm.com>
2024-11-07 17:00:37 -0600

    added sequence block adjustment
    Signed-off-by: Max Crouse <maxwell.crouse@ibm.com>

--- fms_dgt/blocks/compositions/sequence.py
+++ fms_dgt/blocks/compositions/sequence.py
@@ -1,5 +1,5 @@
 # Standard
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Union
 
 # Local
 from fms_dgt.base.block import BaseBlock
@@ -14,7 +14,8 @@ class BlockSequence(BaseBlock):
 
     def __init__(
         self,
-        block_list: List[Dict],
+        block_list: List[Union[Dict, BaseBlock]],
+        block_args_kwargs: List[Dict] = None,
         **kwargs: Any,
     ) -> None:
         super().__init__(**kwargs)
@@ -26,19 +27,58 @@ class BlockSequence(BaseBlock):
                     self.name,
                 )
 
+        self._block_args_kwargs = block_args_kwargs
+
         self._blocks: List[BaseBlock] = [
-            get_block(block_name=block_info[TYPE_KEY], **block_info)
-            for block_info in block_list
+            (
+                get_block(block_name=block[TYPE_KEY], **block)
+                if isinstance(block, dict)
+                else block
+            )
+            for block in block_list
         ]
 
     @property
     def blocks(self):
         return self._blocks
 
-    def execute(self, inputs: DATASET_TYPE):
+    def _get_block_args_kwargs(self, block_args_kwargs: List[Dict]):
+
+        block_args_kwargs = (
+            block_args_kwargs
+            or self._block_args_kwargs
+            or [dict() for _ in self.blocks]
+        )
+
+        if len(block_args_kwargs) != len(self.blocks):
+            raise ValueError(
+                f"When provided, [blocks_args_kwargs] should be the same length as [block_list]"
+            )
+
+        for d in block_args_kwargs:
+            for k in d:
+                if k not in ["args", "kwargs"]:
+                    raise ValueError(
+                        f"All keys to dictionary in [block_args_kwargs] must be either [args, kwargs], instead got [{k}] for dictionary [{d}]"
+                    )
+                for v, t in [("args", list), ("kwargs", dict)]:
+                    if not isinstance(d[v], t):
+                        raise ValueError(
+                            f"Expected {v} to be {t}, instead got {type(d[v])}"
+                        )
+
+        return block_args_kwargs
+
+    def execute(self, inputs: DATASET_TYPE, block_args_kwargs: List[Dict] = None):
+
+        block_args_kwargs = self._get_block_args_kwargs(block_args_kwargs)
+
         block_data = inputs
-        for block in self.blocks:
+        for block, args_kwargs in zip(self.blocks, block_args_kwargs):
             sdg_logger.info("Running block %s", block.name)
             # initial block call will pass custom arg_fields / kwarg_fields / result_field
-            block_data = block(block_data)
+            args, kwargs = args_kwargs.get("args", []), args_kwargs.get(
+                "kwargs", dict()
+            )
+            block_data = block(block_data, *args, **kwargs)
         return block_data
--- tests/blocks/compositions/test_sequence.py
+++ tests/blocks/compositions/test_sequence.py
@@ -1,24 +1,74 @@
 # Local
+from fms_dgt.base.block import DATASET_TYPE, BaseBlock
 from fms_dgt.blocks.compositions.sequence import BlockSequence
 
 
-class TestBlockSequence:
-    def test_flatten(self):
-        flatten_cfg1 = {
-            "type": "flatten_field",
-            "arg_fields": ["arg"],
-            "result_field": "arg",
-        }
-        flatten_cfg2 = {
-            "type": "flatten_field",
-            "arg_fields": ["arg"],
-            "result_field": "arg",
-        }
-        cfgs = [flatten_cfg1, flatten_cfg2]
-        block_sequence = BlockSequence(cfgs)
-        data = [{"arg": [[1, 2, 3], [4, 5, 6]]}]
-        outputs = block_sequence(data)
-        for i in range(1, 7):
-            assert (
-                outputs[i - 1]["arg"] == i
-            ), f"Expected {i} but got {outputs[i-1]['arg']} at position {i-1}"
+def test_flatten():
+    flatten_cfg1 = {
+        "type": "flatten_field",
+        "arg_fields": ["arg"],
+        "result_field": "arg",
+    }
+    flatten_cfg2 = {
+        "type": "flatten_field",
+        "arg_fields": ["arg"],
+        "result_field": "arg",
+    }
+    cfgs = [flatten_cfg1, flatten_cfg2]
+    block_sequence = BlockSequence(cfgs)
+    data = [{"arg": [[1, 2, 3], [4, 5, 6]]}]
+    outputs = block_sequence(data)
+    for i in range(1, 7):
+        assert (
+            outputs[i - 1]["arg"] == i
+        ), f"Expected {i} but got {outputs[i-1]['arg']} at position {i-1}"
+
+
+def test_args_kwargs():
+    flatten_cfg1 = {
+        "type": "flatten_field",
+        "arg_fields": ["arg"],
+        "result_field": "arg",
+    }
+    flatten_cfg2 = {
+        "type": "flatten_field",
+        "arg_fields": ["arg"],
+        "result_field": "arg",
+    }
+    block_sequence = BlockSequence(
+        [TestBlock(**flatten_cfg1), TestBlock(**flatten_cfg2)],
+        block_args_kwargs=[
+            {"args": [1], "kwargs": {"kwarg1": 2}},
+            {"args": [3], "kwargs": {"kwarg1": 4}},
+        ],
+    )
+    data = [{"arg": [[1, 2, 3], [4, 5, 6]]}]
+    outputs = block_sequence(data)
+
+    expected = [{"arg": (3, 4)}]
+    assert (
+        outputs == expected
+    ), f"Incorrect output, expected {expected} but got {outputs}"
+
+    block_args_kwargs = [
+        {"args": [5], "kwargs": {"kwarg1": 6}},
+        {"args": [7], "kwargs": {"kwarg1": 8}},
+    ]
+    outputs = block_sequence(data, block_args_kwargs)
+    expected = [{"arg": (7, 8)}]
+    assert (
+        outputs == expected
+    ), f"Incorrect output, expected {expected} but got {outputs}"
+
+
+class TestBlock(BaseBlock):
+    """Flatten specified args"""
+
+    def execute(self, inputs: DATASET_TYPE, arg1: int, kwarg1: int = None):
+        outputs = []
+        for x in inputs:
+            inp_args, _ = self.get_args_kwargs(x, self._arg_fields, self._kwarg_fields)
+            to_write = (arg1, kwarg1) if arg1 or kwarg1 else inp_args
+            self.write_result(x, to_write)
+            outputs.append(x)
+        return outputs
