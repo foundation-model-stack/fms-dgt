6f9cadc3b6b3b518a7ccd0f17805a4e650e604ff
Max Crouse <maxwell.crouse@ibm.com>
2024-11-26 21:37:11 -0600

    update pipeline to iterate better
    Signed-off-by: Max Crouse <maxwell.crouse@ibm.com>

--- fms_dgt/databuilders/nonstandard/pipeline/task.py
+++ fms_dgt/databuilders/nonstandard/pipeline/task.py
@@ -8,14 +8,6 @@ from fms_dgt.base.task import TransformTask
 class PipelineTransformTask(TransformTask):
     """TransformTask is a subclass of SdgTask that has default values that are more conducive to transformation tasks."""
 
-    def __init__(
-        self,
-        *args,
-        seed_batch_size: int = 100000000,
-        **kwargs,
-    ):
-        super().__init__(*args, seed_batch_size=seed_batch_size, **kwargs)
-
     def instantiate_input_example(self, **kwargs: Any) -> Any:
         return kwargs
 
